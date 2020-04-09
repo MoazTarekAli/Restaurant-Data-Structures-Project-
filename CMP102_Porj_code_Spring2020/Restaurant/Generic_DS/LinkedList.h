b75fd3ae15af0384db11a50c853c47aa33d2d4d7
Muhab Hossam <muhab.hossam13@gmail.com>
2020-04-09 18:54:39 +0200

    Stack/Linkedlist

--- CMP102_Porj_code_Spring2020/Restaurant/Generic_DS/LinkedList.h
+++ CMP102_Porj_code_Spring2020/Restaurant/Generic_DS/LinkedList.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include "Node.h"
 
+#ifndef __LINKEDLIST_H_
+#define __LINKEDLIST_H_
+
 using namespace std;
 
 template <typename T>
@@ -9,11 +12,13 @@ class LinkedList
 {
 private:
 
-	Node<T>* head;
-	Node<T>* tail;
-	int count;
+	Node<T>* head;		// Pointer to the first item in the linkedlist
+	Node<T>* tail;		// Pointer to the last item in the linkedlist
+	int count;			// Counts the number of nodes in the linkedlist
 
 public:
+
+	// Linkedlist constructor that initializes data members to avoid garbage output
 
 	LinkedList()
 	{
@@ -22,62 +27,84 @@ public:
 		count = 0;
 	}
 	
+	// Linkedlist destructor deletes all nodes in the list
+
 	~LinkedList()
 	{
 		DeleteAll();
 	}
 
+	// IsEmpty function returns true for an empty list
+
 	bool IsEmpty()
 	{
 		if (head)
-			return true;
-		else
-			return false;
+			return false;		
+		return true;
 	}
+
+	// GetCount function returns number of nodes in the list
 
 	int GetCount()
 	{
 		return count;
 	}
 
+	// Print function to print linkedlist
+
 	void PrintList()	const
 	{
-		Node<T>* p = head;
+		// Pointer to traverse the list for printing 
 
-		while (p)
+		Node<T>* printTraverse = head;
+
+		while (printer) // exits the loop at the last item in the linkedlist 
 		{
-			cout << p->getItem() << " ";
-			p = p->getNext();
+			cout << printTraverse->getItem() << " ";
+			printTraverse = printTraverse->getNext();
 		}
 	}
 
 
+	// Adds new item to the linkedlist assigning it to the tail of the list
 
 	void Append(T entry)
 	{
+		// Create new node to insert in the list
 
-		Node<T>* pointer = new Node<T>(entry);
+		Node<T>* appender = new Node<T>(entry); 
 		
-		if (!count)
+		if (!count)		// Check if the list is empty
 		{
-			head = tail = pointer;
+			// For the first item in the list the head and tail will point to it
+
+			head = tail = appender; 
 			count++;
 			return;
 		}
 
-		tail->setNext(pointer);
-		tail = pointer;
+		tail->setNext(appender);
+		tail = appender;
 		count++;
 		return;
 	}
 
+	// Insert function to add an item to the linked list in given position
+
 	void Insert(const T item, int index)
 	{
+		// Step 1: Check if the index inserted is out of bounds to terminate if true
+
 		if (!(index >= 0 && index < count))
 			return;
 
-		Node<T>* pointer = head;
-		Node<T>* newitem = new Node<T>(item);
+		Node<T>* traverser = head;				// Node pointer to traverse our list
+		Node<T>* newitem = new Node<T>(item);	// Create a new node to insert into the list
+
+
+
+		//	Step 2: Handling special cases
+		//	Special case handling inserting in the 0 index which replaces the head
 
 		if (index == 0)
 		{
@@ -87,6 +114,8 @@ public:
 			return;
 		}
 
+		//	Special case handling inserting in the last index replacing the head
+
 		if (index == count)
 		{
 			tail->setNext(newitem);
@@ -95,25 +124,34 @@ public:
 			return;
 		}
 
+		//	Step 3: loop to traverse to the given index
+
 		for (int i = 0; i < index - 1; i++)
 		{
-			pointer = pointer->getNext();
+			traverser = traverser->getNext();
 		}
 
-		newitem->setNext(pointer->getNext());
-		pointer->setNext(newitem);
-		count++;
+		//	Step 4: Adding the input item in the input index 
+
+		newitem->setNext(traverser->getNext());
+		traverser->setNext(newitem);
+		
+		//	Step 5: increasing count
+
+		count++; 
 
 	}
 
+	// DeleteAll function to remove all nodes in the linkedlist
+
 	void DeleteAll()
 	{
-		Node<T>* P = head;
+		Node<T>* Deleter = head;
 		while (head)
 		{
-			P = head->getNext();
+			Deleter = head->getNext();
 			delete head;
-			head = P;
+			head = Deleter;
 		}
 		count = 0;
 		tail = head;
@@ -121,31 +159,39 @@ public:
 
 	void Delete(int index)
 	{
+		// Check if the index inserted is out of bounds to terminate if true
+
 		if (!(index >= 0 && index < count))
 			return;
 
-		Node<T>* pointer = head;
+		// Temporary pointer to delete and free the allocated memory
+
+		Node<T>* tempDel = head;
 		
 		if (index == 0)
 		{
 			head = head->getNext();
-			delete pointer;
+			delete tempDel;
 			return;
 		}
 		
-		Node<T>* pointer2 = pointer;
+		// Traversing pointer to go to given index to delete
+
+		Node<T>* traverseToDel = tempDel;
 
 		for (int i = 0; i < index; i++)
 		{
-			pointer2 = pointer;
-			pointer = pointer->getNext();
+			traverseToDel = tempDel;
+			tempDel = tempDel->getNext();
 		}
 
-		pointer2->setNext(pointer->getNext());
-		delete pointer;
-		return;
+		// Deleting action
+
+		traverseToDel->setNext(tempDel->getNext());
+		delete tempDel;
+
 	}
 
 
 };
-
+#endif
---
+++ CMP102_Porj_code_Spring2020/Restaurant/Generic_DS/Stack.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+
+template < typename T>
+class Stack
+{
+	Node<T>* top;
+
+public:
+
+	Stack()
+	{
+		// Default stack constructor to make sure top pointer is NULL in empty stack
+		
+		top = nullptr;
+	}
+	
+	~Stack()	
+	{
+		// Stack destructor that pops all items of stack to delete them
+
+		while (top)
+			pop();
+	}
+
+	void push(T entry)
+	{
+		// Create a 
+
+		Node<T>* pusher = new Node<T>(entry);
+
+		if (IsEmpty())	// Function returns true for an empty stack
+		{
+			// validation for the first item in the stack
+
+			top = pusher;
+		}
+		else {
+
+			// Store the current top value as the next pointer of the node
+
+			pusher->setNext(top);
+
+			// Changes the current top to the last entered item as stack is LIFO
+
+			top = pusher;
+		}
+	}
+	
+	// Checks if the Stack is Empty 
+
+	bool IsEmpty()
+	{
+
+		if (top)			// Checks that the top doesn't point to a null pointer
+			return false;
+
+		return true;		// returns true when the top pointer is null
+	}
+
+	// "Pops" the stack returning the item on the "top" of the stack which is the last entered item 
+
+	T pop()
+	{
+		if (IsEmpty())	// Validation incase the stack is empty 
+			return -1;	// return -1 in case of empty
+
+		T item = top->getItem();	// stores the item in the top of the stack
+		Node<T>* tempDel = top;		// Temporary pointer to delete the top
+		top = top->getNext();		// moves the top of the stack
+		delete tempDel;				// Delete action
+		return item;
+	}
+
+	T peek()
+	{
+		if (top)			// Checks if the stack is empty to avoid returning a NULL ptr
+			return top;		// Returns the top item in the stack
+		
+		// If the stack is empty it should return -1
+
+		return -1;
+	}
+};
+
